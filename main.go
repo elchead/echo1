9fa7e600fc5320c6c81c5ab5aa8c63a10e74d6f7
Adrian <stobbe.adrian@gmail.com>
2022-12-12 15:44:25 +0100

    time to connect to server ~ 400microS

--- main.go
+++ main.go
@@ -8,51 +8,63 @@
 package main
 
 import (
-    "net"
-    "bufio"
-    "strconv"
-    "fmt"
+	"bufio"
+	"fmt"
+	"net"
+	"strconv"
+	"time"
 )
 
 const PORT = 3540
 
 func main() {
 	fmt.Println("Listen on 3540")
-    server, err := net.Listen("tcp", ":" + strconv.Itoa(PORT))
-    if server == nil {
-        panic(err)
-    }
-    conns := clientConns(server)
-    for {
-        go handleConn(<-conns)
-    }
+	now := time.Now()
+	go func() {
+		server, err := net.Listen("tcp", ":"+strconv.Itoa(PORT))
+		if server == nil {
+			panic(err)
+		}
+		conns := clientConns(server)
+		for {
+			go handleConn(<-conns)
+		}
+	}()
+	// wait for connection
+	for {
+		_, err := net.Dial("tcp", "localhost:3540")
+		if err == nil {
+			break
+		}
+	}
+	fmt.Println("Time:", time.Since(now))
 }
 
 func clientConns(listener net.Listener) chan net.Conn {
-    ch := make(chan net.Conn)
-    i := 0
-    go func() {
-        for {
-            client, err := listener.Accept()
-            if client == nil {
-                fmt.Println(err)
-                continue
-            }
-            i++
-            fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
-            ch <- client
-        }
-    }()
-    return ch
+	ch := make(chan net.Conn)
+	i := 0
+	go func() {
+		for {
+			client, err := listener.Accept()
+			if client == nil {
+				fmt.Println(err)
+				continue
+			}
+			i++
+			fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
+			ch <- client
+		}
+	}()
+	return ch
 }
 
 func handleConn(client net.Conn) {
-    b := bufio.NewReader(client)
-    for {
-        line, err := b.ReadBytes('\n')
-        if err != nil { // EOF, or worse
-            break
-        }
-        client.Write(line)
-    }
+	b := bufio.NewReader(client)
+	for {
+		line, err := b.ReadBytes('\n')
+		if err != nil { // EOF, or worse
+			break
+		}
+		client.Write(line)
+	}
 }
